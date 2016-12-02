0a0058b61930c1c6baeadde5e4cee125e0c94ec0
introlist <gilflo01@hotmail.com>
2016-12-02 17:26:02 -0600

    Revert "Quitada Base de Datos innecesaria, agregada la de la ultima expo"
    This reverts commit 3b0adc4ce5ce61a77a3ab7109c3795a3ef074ba9.

--- katakdbv00.sql
+++ katakdbv00.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2016 a las 10:18:34
+-- Tiempo de generación: 13-10-2016 a las 03:18:49
 -- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 7.0.9
+-- Versión de PHP: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,34 +30,9 @@ CREATE TABLE `clientes` (
   `id` int(10) NOT NULL,
   `nombre` varchar(127) NOT NULL,
   `telefono` varchar(127) NOT NULL,
+  `celular` varchar(127) NOT NULL,
   `direccion` varchar(350) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`id`, `nombre`, `telefono`, `direccion`) VALUES
-(3, 'Mario Lopez', '99914317421', 'Francisco de Montejo');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `idProducto` int(11) NOT NULL,
-  `nombreProducto` varchar(50) NOT NULL,
-  `precioProducto` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`idProducto`, `nombreProducto`, `precioProducto`) VALUES
-(1, 'Charritos', 50);
 
 --
 -- Índices para tablas volcadas
@@ -77,7 +52,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
